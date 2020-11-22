475927d6012e229ce0e35652d6baadff3c32f50c
Shinto C V <shinto@c3m.io>
2020-11-23 00:53:19 +0530

    Initialize project

---
+++ tetris.py
@@ -0,0 +1,20 @@
+""" The game of Tetris 
+
+The game happens in a grid of size R x C. Each cell will have a color. Each shape like
+S, T, I, L, etc. will represented by their own colors. Each shape has varying number of rotations.
+For example L has 4 and O has only 1. We have to also represent locked positions.
+
+Get random shaped pieces. 
+Set speed of falling down. Represent falling down.
+Keys: L, R, U, D
+
+U: rotate the piece
+
+Start falling piece on the center.
+
+Check if the move is valid.
+
+Clear rows when the row is filled.
+
+Check lost.
+"""
